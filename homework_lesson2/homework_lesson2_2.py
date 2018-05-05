d3995bfc802a177f9e2655d61d99068617d57163
SunPrime <sunprime@ukr.net>
2018-05-05 12:29:09 +0300

    тестовый фреймворк, передаю в тест любые аргументы

--- homework_lesson2/homework_lesson2_2.py
+++ homework_lesson2/homework_lesson2_2.py
@@ -1,18 +1,21 @@
+import unittest
 def func_sqrt_test(func4test):
-    import unittest
-    class Failfunc_sqrt_test(unittest.TestCase):
-        def test_func4test(self):
-            self.assertEqual(func4test(4, 2), 16)
+    def testtest(x, y):
+        class Failfunc_sqrt_test(unittest.TestCase):
+            def test_func4test(self):
+                self.assertEqual(func4test(x, y), x ** y)
 
-    suite = unittest.TestLoader().loadTestsFromTestCase(Failfunc_sqrt_test)
-    unittest.TextTestRunner(verbosity=2).run(suite)
-    return func4test
+        suite = unittest.TestLoader().loadTestsFromTestCase(Failfunc_sqrt_test)
+        unittest.TextTestRunner(verbosity=2).run(suite)
+        return func4test(x, y)
+    return testtest
 
 @func_sqrt_test
 def func_sqrt(a, b):
     return a ** b
 
-num1 = 20
+
+num1 = 25
 num2 = 3
 res = func_sqrt(num1, num2)
 print(res)
