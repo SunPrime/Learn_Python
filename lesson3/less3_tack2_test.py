be50d49beb1748960c9b344db64ec35c46cb1fd0
SunPrime <sunprime@ukr.net>
2018-05-14 16:17:52 +0300

    homework, lesson 4, adding test for subclass for cargo

--- lesson3/less3_tack2_test.py
+++ lesson3/less3_tack2_test.py
@@ -1,6 +1,7 @@
 import unittest
 
-from lesson3.less3_tack2 import Ticket, Fuel, Staff, Airplane, Flight, Air_company
+from lesson3.less3_tack2 import Ticket, Fuel, Staff, Airplane, Flight, Air_company, Freight, Cargo_Airplane, \
+    Cargo_Fligth
 
 
 class TestCase_less3_tack2(unittest.TestCase):
@@ -18,6 +19,19 @@ class TestCase_less3_tack2(unittest.TestCase):
             flight444.add_ticket(ticket1)
         mau = Air_company()
         mau.add_flight(flight444)
+        freight1 = Freight(20000, 10)  # вес и цена за 1 кг груза
+        freight2 = Freight(50000, 20)
+        freight3 = Freight(10000, 30)
+        fuel_cargo = Fuel(20)
+        staff_crew = Staff(1000)
+        plane_An225 = Cargo_Airplane(10, fuel_cargo, 5, 250000)
+        flight555 = Cargo_Fligth(1200)
+        flight555.set_airplane(plane_An225)
+        flight555.add_staff(staff_crew)
+        flight555.add_cargo(freight1)
+        flight555.add_cargo(freight2)
+        flight555.add_cargo(freight3)
+        mau.add_cargo_flight(flight555)
         mau.total_profit()
 
 
