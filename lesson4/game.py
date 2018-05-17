dcbdb76874f7111deebcb630d54340ac1076e807
SunPrime <sunprime@ukr.net>
2018-05-17 15:42:18 +0300

    scratch game

---
+++ lesson4/game.py
@@ -0,0 +1,58 @@
+class Game:
+    def __init__(self, player1, player2):
+        self.player1 = player1
+        self.player2 = player2
+
+    def start_game(self):
+        while True:
+            if not self.player1.is_free_cell():
+                return "Draw"
+            self.player1.step()
+            if self.player1.is_winner():
+                return "Player1 is Winner"
+            if not self.player2.is_free_cell():
+                return "Draw"
+            self.player2.step()
+            if self.player2.is_winner():
+                return "Player2 is Winner"
+
+
+class Player:
+    def __init__(self, field, mark):
+        self.field = field
+        self.mark = mark
+
+    def is_free_cell(self):
+        return self.field.is_free_cell()
+
+    def step(self):
+        pass
+
+    def is_winner(self):
+        return self.field.is_line(self.mark)
+
+
+class Human(Player):
+    def __init__(self, field, mark):
+        Player.__init__(self, field, mark)
+
+    def step(self):
+        x = int(input())
+        y = int(input())
+        self.field.change_status(x, y, self.mark)
+        self.field.show()
+
+
+class Computer(Player):
+    def __init__(self, field, mark):
+        Player.__init__(self, field, mark)
+
+    def step(self):
+        x, y = self.field.get_free_cell()
+        self.field.change_status(x, y, self.mark)
+        self.field.show()
+
+
+class Cell:
+    def __init__(self):
+        self.status = 'p'
