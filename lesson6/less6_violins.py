df8678f5794f5eb72b89d54574ede49ad54e8bcf
sunprime <sunprime@ukr.net>
2018-06-07 09:34:22 +0300

    add while

--- lesson6/less6_violins.py
+++ lesson6/less6_violins.py
@@ -21,33 +21,41 @@ class Violinist(threading.Thread):
         i = 0
         while i < 6:
             self.semaphore.acquire()
-            self.get_instrument()
+            viol, bow = self.get_instrument()
             time.sleep(random.randint(1, 5))
-            self.put_instrument()
+            self.put_instrument(viol, bow)
             self.semaphore.release()
+            time.sleep(2)
             i += 1
 
     def get_instrument(self):
         self.mutex_violin.acquire()
-        self.queue_violin.get('violin')
-        print(self.name + ' take violin')
+        while (self.queue_violin.qsize() == 0):
+            self.mutex_violin.wait()
+        viol = self.queue_violin.get()
+        print(self.name + ' take ' + viol)
         self.mutex_violin.notify()
         self.mutex_violin.release()
+
         self.mutex_bow.acquire()
-        self.queue_bow.get('bow')
-        print(self.name + ' take bow')
+        while (self.queue_bow.qsize() == 0):
+            self.mutex_bow.wait()
+        bow = self.queue_bow.get()
+        print(self.name + ' take ' + bow)
         self.mutex_bow.notify()
         self.mutex_bow.release()
 
-    def put_instrument(self):
+        return viol, bow
+
+    def put_instrument(self, viol, bow):
         self.mutex_violin.acquire()
-        self.queue_violin.put('violin')
-        print(self.name + ' put violin')
+        self.queue_violin.put(viol)
+        print(self.name + ' put ' + viol)
         self.mutex_violin.notify()
         self.mutex_violin.release()
         self.mutex_bow.acquire()
-        self.queue_bow.put('bow')
-        print(self.name + ' put bow')
+        self.queue_bow.put(bow)
+        print(self.name + ' put ' + bow)
         self.mutex_bow.notify()
         self.mutex_bow.release()
 
@@ -56,15 +64,15 @@ pool_sema = BoundedSemaphore(value=maxconnections)
 
 mutex_violin = threading.Condition()
 queue_violin = queue.Queue(3)
-queue_violin.put('violin')
-queue_violin.put('violin')
-queue_violin.put('violin')
+queue_violin.put('violin1')
+queue_violin.put('violin2')
+queue_violin.put('violin3')
 
 mutex_bow = threading.Condition()
 queue_bow = queue.Queue(3)
-queue_bow.put('bow')
-queue_bow.put('bow')
-queue_bow.put('bow')
+queue_bow.put('bow1')
+queue_bow.put('bow2')
+queue_bow.put('bow3')
 
 mutex = threading.Condition()
 
@@ -81,9 +89,3 @@ violinist3.start()
 violinist4.start()
 violinist5.start()
 violinist6.start()
-violinist1.join()
-violinist2.join()
-violinist3.join()
-violinist4.join()
-violinist5.join()
-violinist6.join()
