d560a3cc5593aa1ace6c5c9bff8f9319df8323b0
sunprime <sunprime@ukr.net>
2018-06-11 00:10:49 +0300

    delete semaphore

--- lesson6/less6_violins.py
+++ lesson6/less6_violins.py
@@ -1,14 +1,11 @@
 import threading
-import random
 import time
 import queue
-from threading import BoundedSemaphore
 
 
 class Violinist(threading.Thread):
-    def __init__(self, semaphore, queue_violin, queue_bow, mutex_violin, mutex_bow, name):
+    def __init__(self, queue_violin, queue_bow, mutex_violin, mutex_bow, name):
         threading.Thread.__init__(self)
-        self.semaphore = semaphore
         self.queue_violin = queue_violin
         self.queue_bow = queue_bow
         self.mutex_violin = mutex_violin
@@ -18,11 +15,9 @@ class Violinist(threading.Thread):
     def run(self):
         i = 0
         while i < 6:
-            self.semaphore.acquire()
             viol, bow = self.get_instrument()
-            time.sleep(random.randint(1, 5))
+            time.sleep(2)
             self.put_instrument(viol, bow)
-            self.semaphore.release()
             time.sleep(2)
             i += 1
 
@@ -40,31 +35,29 @@ class Violinist(threading.Thread):
         while (queue.qsize() == 0):
             mutex.wait()
         part_instrument = queue.get()
-        print(self.name + ' take ' + part_instrument)
-        mutex.notify_all()
+        mutex.notify()
         mutex.release()
+        print(self.name + ' take ' + part_instrument)
         return part_instrument
 
     def put_instrument4thread(self, mutex, queue, part_instrument):
         mutex.acquire()
         queue.put(part_instrument)
-        print(self.name + ' put ' + part_instrument)
-        mutex.notify_all()
+        mutex.notify()
         mutex.release()
+        print(self.name + ' put ' + part_instrument)
 
-
-maxconnections = 3
-pool_sema = BoundedSemaphore(value=maxconnections)
 
 mutex_violin = threading.Condition()
 queue_violin = queue.Queue(3)
+
 mutex_bow = threading.Condition()
 queue_bow = queue.Queue(3)
 
 for item in range(1, 4):
-    queue_violin.put('violin' + str(item))
-    queue_bow.put('bow' + str(item))
+     queue_violin.put('violin' + str(item))
+     queue_bow.put('bow' + str(item))
 
 for item in range(1, 7):
-    violinist = Violinist(pool_sema, queue_violin, queue_bow, mutex_violin, mutex_bow, ('Violinist' + str(item)))
+    violinist = Violinist(queue_violin, queue_bow, mutex_violin, mutex_bow, ('Violinist' + str(item)))
     violinist.start()
