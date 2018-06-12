c7aeed8496aa73f0f872c1849c967559efb45689
sunprime <sunprime@ukr.net>
2018-06-12 21:07:38 +0300

    task is about 6 violinists and 3 violins via processing

---
+++ lesson7/less7_homework/less7_violinist_mp.py
@@ -0,0 +1,29 @@
+import multiprocessing, time, random
+from multiprocessing import current_process
+
+def play_violin(violin, bow):
+    name = current_process().name
+    time.sleep(2)
+    res = 'Voilinist' + name.replace('SpawnPoolWorker-','') +' plays the ' + violin + ' and ' + bow
+    return res
+
+
+def main():
+    start = time.clock()
+    violins = ['violin1', 'violin2', 'violin3']
+    bows = ['bow1', 'bow2', 'bow3']
+
+    pool = multiprocessing.Pool(6)
+    i = 0
+    while i < 6:
+        res = pool.starmap(play_violin, zip(violins, bows))
+        for index in range(len(res)):
+            print(res[index])
+        i += 1
+
+    end = time.clock()
+    result_time = end - start
+    print('time is %f' % result_time)
+
+if __name__ == '__main__':
+    main()
